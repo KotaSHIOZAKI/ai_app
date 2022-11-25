ad843e863946a5b413c49df09b2427e78f089913
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-11-25 10:02:31 +0900

    コメントの記載、画像認識ページのレイアウト修正

--- main.py
+++ main.py
@@ -13,19 +13,24 @@ def index():
 
 @app.route('/image', methods=['GET', 'POST'])
 def image():
+    #画像認識ページ
     if request.method == 'POST':
-        f = request.files.get('image')
+        #POSTされたとき
+        
         #ファイル名を取得
+        f = request.files.get('image')
         filename = secure_filename(f.filename)
         #ファイルを保存するディレクトリを指定
         filepath = 'static/image/' + filename
         #ファイルを保存する
         f.save(filepath)
 
+        #検出対象（０＝人物、１＝物、２＝文字列）
         t = int(request.form.get('type'))
 
         result_url, result_str = func.detect_text(filename, t)
     else:
+        #GETされたとき
         result_url = ''
         result_str = []
     return render_template('rekognition.html', result=result_url, result_str=result_str)
