6ce792f903f316f78885c3b18b922a937f2fb263
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-11-29 10:19:16 +0900

    インデックスページの構築

--- translate.py
+++ translate.py
@@ -24,25 +24,25 @@ languages = {
     'zh' : '中国語（簡体字）',
     'zh-TW' : '中国語（繁体字）'
 }
-voice_id_dict = {
-    "ar" : "Zeina",
-    "da" : "Naja",
-    "en" : "Joanna",
-    "es" : "Lucia",
-    "fr" : "Ceilne",
-    "de" : "Marlene",
-    "it" : "Carla",
-    "ja" : "Mizuki",
-    "ko" : "Seoyeon",
-    "nl" : "Lotte",
-    "pl" : "Ewa",
-    "pt" : "Vitoria",
-    "ru" : "Tatyana",
-    "sv" : "Astrid",
-    "tr" : "Filiz",
-    "zh" : "Zhiyu",
-    "zh-TW" : "Zhiyu"
-}
+# voice_id_dict = {
+#     "ar" : "Zeina",
+#     "da" : "Naja",
+#     "en" : "Joanna",
+#     "es" : "Lucia",
+#     "fr" : "Ceilne",
+#     "de" : "Marlene",
+#     "it" : "Carla",
+#     "ja" : "Mizuki",
+#     "ko" : "Seoyeon",
+#     "nl" : "Lotte",
+#     "pl" : "Ewa",
+#     "pt" : "Vitoria",
+#     "ru" : "Tatyana",
+#     "sv" : "Astrid",
+#     "tr" : "Filiz",
+#     "zh" : "Zhiyu",
+#     "zh-TW" : "Zhiyu"
+# }
 
 #======= 翻訳 =======
 def translate(sequence, lang1, lang2):
@@ -54,21 +54,30 @@ def translate(sequence, lang1, lang2):
             Text=sequence, SourceLanguageCode=lang1, TargetLanguageCode=lang2
         )['TranslatedText']
 
-        #音声合成（上書きされずに前回保存した音声が流れてしまう）
-        if lang2 in voice_id_dict:
-            polly = boto3.client('polly')
-            read_text = polly.synthesize_speech(
-                Text=result, OutputFormat='mp3', VoiceId=voice_id_dict[lang2]
-            )
-            #音声ファイルの保存
-            path = 'static/audio/read_text.mp3'
-            with contextlib.closing(read_text['AudioStream']) as stream:
-                with open(path, 'wb') as file:
-                    file.write(stream.read())
-        else:
-            path = ''
+        # #音声合成
+        # if lang2 in voice_id_dict:
+        #     polly = boto3.client('polly')
+        #     read_text = polly.synthesize_speech(
+        #         Text=result, OutputFormat='mp3', VoiceId=voice_id_dict[lang2]
+        #     )
+        #     #音声ファイルの保存
+        #     path = 'static/audio/read_text.mp3'
+        #     with contextlib.closing(read_text['AudioStream']) as stream:
+        #         with open(path, 'wb') as file:
+        #             file.write(stream.read())
+        # else:
+        #     path = ''
+        path = ''
         
         #翻訳メモ
+        """
+        例）
+        日本語
+        こんにちは
+
+        英語
+        Hi
+        """
         with open('static/memo/translate_out.txt', 'w', encoding='utf-8') as file_out:
             file_out.write(languages[lang1] + '\n')
             file_out.write(sequence + '\n\n')
